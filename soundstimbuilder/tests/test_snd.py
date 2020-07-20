6070e4cedcda16bf4c0740d3d0249c0503b1c8e1
Gabriel <gabriel@gbeckers.nl>
2020-07-20 13:32:27 +0200

    fix test fs param

--- soundstimbuilder/tests/test_snd.py
+++ soundstimbuilder/tests/test_snd.py
@@ -7,5 +7,5 @@ from soundstimbuilder import Snd
 class TestSnd(unittest.TestCase):
 
     def test_numpyframes(self):
-        self.assertIsInstance(Snd(frames=[1,2,3]).frames, np.ndarray)
+        self.assertIsInstance(Snd(frames=[1,2,3], fs=1).frames, np.ndarray)
 
